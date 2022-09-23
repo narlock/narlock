9ad6c76d11b449afa73cef2105997597e08282dc
narlock <anthonynarlock@outlook.com>
2022-09-23 11:02:32 -0500

    added blog page

---
+++ js/blog.js
@@ -0,0 +1,8 @@
+function showHome() {
+    document.getElementById('goHome').style.display = 'block';
+    console.log("hello world");
+}
+
+function hideHome() {
+    document.getElementById('goHome').style.display = 'none';
+}
